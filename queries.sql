f7530c7cb9ed4d84623bfcf7f2eaa162cbbe66c5
marciele <marciele.inacy@gmail.com>
2025-03-19 15:54:48 +0100

    Query update and chart inclusion
    - Fixed the query to correctly calculate the average number of passengers using PARTITION BY.
    - Added passenger chart (media_passengers.png) in the images folder.
    - Organized the project structure.

--- queries.sql
+++ queries.sql
@@ -2,23 +2,21 @@
 -- for aircraft with a capacity greater than 200 passengers.
 
 SELECT 
-   ba_aircraft.manufacturer, -- Select the manufacturer of the aircraft
-   ROUND(AVG(ba_flights.total_passengers), 1) -- Calculate the average number of passengers, rounded to 1 decimal
-   
+   ba_aircraft.manufacturer, 
+   Ba_flights.total_passengers,   -- Include total passengers as a field
+   ba_fuel_efficiency.capacity,     -- Include aircraft capacity
+   ROUND(AVG(ba_flights.total_passengers) OVER(PARTITION BY ba_aircraft.manufacturer), 1) AS avg_passengers_per_flight
+   -- Use a window function (OVER PARTITION BY) to keep total_passengers visible
+
 FROM ba_flights 
--- Joining ba_aircraft and ba_fuel_efficiency tables
 INNER JOIN ba_aircraft
    ON ba_flights.flight_id = ba_aircraft.flight_id
 
 INNER JOIN ba_fuel_efficiency 
    ON ba_aircraft.ac_subtype = ba_fuel_efficiency.ac_subtype
 
--- Apply filter for Boeing aircraft with capacity greater than 200 passengers
 WHERE ba_fuel_efficiency.capacity > 200 
-   AND ba_aircraft.manufacturer = 'Boeing'
-
--- Group results by manufacturer
-GROUP BY ba_aircraft.manufacturer;
+   AND ba_aircraft.manufacturer = 'Boeing';
 
 
 
